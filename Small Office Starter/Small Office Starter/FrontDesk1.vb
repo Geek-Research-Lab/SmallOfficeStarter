f1db1c43fccdf6a593ee0265cdc5d7ff1dcc40b6
ashumeow <ashumeow@live.com>
2020-02-07 20:10:03 +0300

    update

---
+++ Small Office Starter/Small Office Starter/FrontDesk1.vb
@@ -0,0 +1,35 @@
+﻿Public Class FrontDesk1
+    ' exit
+    Private Sub Exit_Button_Click(sender As Object, e As EventArgs) Handles Exit_Button.Click
+        End
+    End Sub
+    ' about
+    Private Sub FrontDesk_Footer_About_Click(sender As Object, e As EventArgs) Handles FrontDesk_Footer_About.Click
+        Dim FrontDesk_About As AboutBox1 = New AboutBox1()
+        FrontDesk_About.Show()
+    End Sub
+    ' appearance
+    Private Sub FrontDesk_Footer_Appearance_Click(sender As Object, e As EventArgs) Handles FrontDesk_Footer_Appearance.Click
+        Dim FrontDeskControl1 As FrontDesk_Appearance = New FrontDesk_Appearance()
+        FrontDeskControl1.Show()
+    End Sub
+    ' company info
+    Private Sub FrontDesk_Footer_CompanyInfo_Click(sender As Object, e As EventArgs) Handles FrontDesk_Footer_CompanyInfo.Click
+        Dim FrontDeskControl2 As CompanyInfo = New CompanyInfo()
+        FrontDeskControl2.Show()
+    End Sub
+    ' language
+    Private Sub FrontDesk_Footer_Language_Click_1(sender As Object, e As EventArgs) Handles FrontDesk_Footer_Language.Click
+        Dim FrontDeskControl3 As LanguagePicker = New LanguagePicker()
+        FrontDeskControl3.Show()
+    End Sub
+    ' init load
+    Private Sub FrontDesk_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ''
+        '' todo : add some code
+        ''
+    End Sub
+End Class
+''
+'' ashumeow
+''
---
+++ Small Office Starter/Small Office Starter/FrontDesk_Appearance.vb
@@ -0,0 +1,36 @@
+﻿Public Class FrontDesk_Appearance
+    ' theme 1
+    Private Sub FrontDesk_Theme_1_Click(sender As Object, e As EventArgs) Handles FrontDesk_Theme_1.Click
+
+    End Sub
+    ' theme 2
+    Private Sub FrontDesk_Theme_2_Click(sender As Object, e As EventArgs) Handles FrontDesk_Theme_2.Click
+
+    End Sub
+    ' theme 3
+    Private Sub FrontDesk_Theme_3_Click(sender As Object, e As EventArgs) Handles FrontDesk_Theme_3.Click
+
+    End Sub
+    ' theme 4
+    Private Sub FrontDesk_Theme_4_Click(sender As Object, e As EventArgs) Handles FrontDesk_Theme_4.Click
+
+    End Sub
+    ' theme 5
+    Private Sub FrontDesk_Theme_5_Click(sender As Object, e As EventArgs) Handles FrontDesk_Theme_5.Click
+
+    End Sub
+    ' theme 6
+    Private Sub FrontDesk_Theme_6_Click(sender As Object, e As EventArgs) Handles FrontDesk_Theme_6.Click
+
+    End Sub
+    ' update / close
+    Private Sub FrontDesk_Theme_UpdateClose_Click(sender As Object, e As EventArgs) Handles FrontDesk_Theme_UpdateClose.Click
+        ''
+        '' todo : add more code
+        ''
+        Close()
+    End Sub
+End Class
+''
+'' ashumeow
+''
